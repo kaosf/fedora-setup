26a32ea798e512a004244eff073e3fe8e86700ca
ka <ka.kaosf@gmail.com>
2013-09-14 17:30:45 +0900

    modify setup.sh for local installation

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@ echo "/usr/bin/zsh" | sudo tee -a /etc/shells
 chsh -s /usr/bin/zsh
 
 sudo yum install ibus-mozc
+
+touch $HOME/.zshenv && chmod 600 $HOME/.zshenv
+
+# for local install
+mkdir -p $HOME/local/src
+mkdir -p $HOME/local/bin
+echo 'export PATH=$HOME/local/bin:$PATH' >> $HOME/.zshenv
