4a8eb019412e36841ccda02f6780a979d899377c
ka <ka.kaosf@gmail.com>
2014-07-25 13:01:22 +0900

    Update go-setup.sh
    ref. ubuntu-setup/a4b733e6d728deaa89e8e6d72db335385e9cb378/go-setup.sh

--- go-setup.sh
+++ go-setup.sh
@@ -1,23 +1,17 @@
 # ref. http://golang.org/doc/install
-wget http://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz
-tar xzf go1.1.2.linux-amd64.tar.gz
+VERSION=1.3
+wget http://golang.org/dl/go${VERSION}.linux-amd64.tar.gz
+tar xzf go${VERSION}.linux-amd64.tar.gz
 mv go $HOME/.go
-echo 'export GOROOT=$HOME/.go' >> .zshenv
-echo 'export PATH=$PATH:$GOROOT/bin' >> .zshenv
-echo 'export GOPATH=$HOME/.gocode' >> .zshenv
+echo 'export GOROOT=$HOME/.go' >> $HOME/.zshenv
+echo 'export PATH=$PATH:$GOROOT/bin' >> $HOME/.zshenv
+echo 'export GOPATH=$HOME/.gocode' >> $HOME/.zshenv
 echo 'export PATH=$HOME/.gocode/bin:$PATH' >> $HOME/.zshenv
 source $HOME/.zshenv
+rm -f go${VERSION}.linux-amd64.tar.gz
 
-# ref. https://github.com/heroku/hk
-# Mercurial ("hg" command) is required
-# Heroku Toolbelt ("heroku" command) is required
-sudo yum install mercurial
-go get github.com/heroku/hk
-APITOKEN=`heroku auth:token`
-echo 'machine api.heroku.com'         >  $HOME/.netrc
-echo '  login '$EMAIL                 >> $HOME/.netrc
-echo '  password '$APITOKEN           >> $HOME/.netrc
-echo 'machine core.heroku.com'        >> $HOME/.netrc
-echo '  login '$EMAIL                 >> $HOME/.netrc
-echo '  password '$APITOKEN           >> $HOME/.netrc
-chmod 600 $HOME/.netrc
+# ref. https://github.com/heroku/hk#mac-os-x-linux-bsd
+L=$HOME/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
+
+# ref. https://github.com/peco/peco#go-get
+go get github.com/peco/peco/cmd/peco
