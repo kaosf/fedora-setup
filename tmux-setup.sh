1a142645c4f7acc1715036d15a8eb461844a3c1a
ka <ka.kaosf@gmail.com>
2015-09-27 15:17:34 +0900

    Bump Tmux version to 2.0

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,9 +1,9 @@
 sudo yum install libevent-devel
 
 cd $HOME/local/src
-wget http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz
-tar xzf tmux-1.9a.tar.gz
-cd tmux-1.9a
+wget http://downloads.sourceforge.net/tmux/tmux-2.0.tar.gz
+tar xf tmux-2.0.tar.gz
+cd tmux-2.0
 ./configure --prefix=$HOME/local
 make
 make install
